33ffc2af451fdafa0c78bb7935ee932ded8bfbe1
SHALAISHA <112056238+BEBYSHA@users.noreply.github.com>
2023-01-25 19:06:19 +0700

    Create B01.java

--- B01.java
+++ B01.java
@@ -1,11 +1,3 @@
-/*
-TUGAS PRAKTIKUM PERTEMUAN KE-02
-PEMROGRAMAN BERORIENTASI OBJEK, GANJIL 2022/2023
-NPM     : 2113020201
-Nama    : Shalaisha Amelia Putri Gemini
-Kelas   : 2A
- */
-
 import java.util.*;
 public class B01 {
     Scanner input = new Scanner (System.in);
