bdfb959416ddf144b57fc107a931f6bb25ef5801
SHALAISHA <112056238+BEBYSHA@users.noreply.github.com>
2023-01-25 19:02:05 +0700

    Update 0204.java

--- 0204.java
+++ 0204.java
@@ -1,12 +1,3 @@
-/*
-TUGAS PERTEMUAN KE-02
-PEMROGRAMAN BERORIENTASI OBJEK, GANJIL 2022/2023
-NPM      : 2113020201
-Nama     : Shalaisha Amelia Putri Gemini
-Kelompok : 5
-Kelas    : 2A
- */
-
 import java.util.Random;
 import java.util.Scanner;
 
